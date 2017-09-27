8a54d33b6b6608e66698dfe8f7080e978f374121
Zhen <zhen.lu@kaust.edu.sa>
2017-09-27 12:41:09 +0300

    wall function case

---
+++ bak_SwB_s_PIMPLE_Wall/run_shaheen.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#SBATCH --account=k1242
+#SBATCH --job-name=SwBdNR
+#SBATCH --nodes=8
+#SBATCH --ntasks-per-node=32
+#SBATCH --ntasks=256
+#SBATCH --ntasks-per-socket=16
+#SBATCH --time=3-00:00:00
+#SBATCH -e job%J.err
+#SBATCH -o job%J.out
+#SBATCH --partition=72hours
+#SBATCH --qos=72hours
+
+OMP_NUM_THREADS=1
+
+sed "s/@NUMSUBDOM@/256/g" \
+    system/decomposeParDict_template > system/decomposeParDict
+decomposePar
+
+sed -e "s/@STARTTIME@/startTime/g" -e "s/@ENDTIME@/0.001/g" \
+    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.001/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g"  \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cp system/controlDict system/log1_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.01/g" \
+    -e "s/@DELTAT@/1e-6/g" -e "s/@WRITEINTERVAL@/0.009/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cp system/controlDict system/log2_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.10/g" \
+    -e "s/@DELTAT@/1e-6/g" -e "s/@WRITEINTERVAL@/0.01/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+#cp system/controlDict system/log3_controlDict
+#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.15/g" \
+#    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+#    -e "s/@WRITEFORMAT@/binary/g" \
+#    -e "s/@ENABLED@/false/g" \
+#    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+#    system/controlDict_template > system/controlDict
+#srun --hint=nomultithread pimpleFoam -parallel
+#
+## START TAKING AVERAGES
+#
+#cp system/controlDict system/log4_controlDict
+#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.20/g" \
+#    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+#    -e "s/@WRITEFORMAT@/binary/g" \
+#    -e "s/@ENABLED@/true/g" \
+#    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+#    system/controlDict_template > system/controlDict
+#srun --hint=nomultithread pimpleFoam -parallel
+#
+#
+## CONTINUE AVERAGING
+#cp system/controlDict system/log5_controlDict
+#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.25/g" \
+#    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+#    -e "s/@WRITEFORMAT@/binary/g" \
+#    -e "s/@ENABLED@/true/g" \
+#    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+#    system/controlDict_template > system/controlDict
+#srun --hint=nomultithread pimpleFoam -parallel
