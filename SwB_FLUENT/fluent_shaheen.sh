e0b315faa7f0eb02c7572b6f61c9d650bb697e8e
Zhen <zhen.lu@kaust.edu.sa>
2017-09-28 12:50:58 +0300

    update folder names

--- SwB_FLUENT/fluent_shaheen.sh
+++ SwB_FLUENT/fluent_shaheen.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #SBATCH --account=k1242
-#SBATCH -N 1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --ntasks-per-socket=16
+#SBATCH --partition=workq
 #SBATCH -t 1-00:00:00
-#SBATCH -J SwBsL3WALE
+#SBATCH -J SwBsL3WMLES
 #SBATCH -e job%J.err
 #SBATCH -o job%J.out
-#SBATCH --partition=workq
 
 module load ansys/v170
 export FLUENT_ENABLE_SLURM_SUPPORT=1
 
-cd /scratch/luz0a/SwB/SwBs_L-300360R100180_16M_FLUENT_WALE
-
 ln -s /opt/cray/mpt/7.2.4/gni/mpich2-cray/8.3/lib/libmpich.so libmpich.so.1
 
-fluent -t32 3ddp -mpi=cray -g -nmon -i SwBs_L-300_WALE.jou
+fluent -t32 3ddp -mpi=cray -g -nmon -i SwBs_L-300_WMLES.jou
 exit
 OK
 EOF
