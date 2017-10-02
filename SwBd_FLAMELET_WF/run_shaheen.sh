dc35c780327c06711cb01fd9a6d7b6dd2a46d322
Zhen <zhen.lu@kaust.edu.sa>
2017-10-02 13:44:19 +0300

    job script, filter for wall function case

--- SwBd_FLAMELET_WF/run_shaheen.sh
+++ SwBd_FLAMELET_WF/run_shaheen.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 #SBATCH --account=k1242
-#SBATCH --job-name=SwBdFLT
+#SBATCH --job-name=SwBdFLTL4
 #SBATCH --nodes=8
 #SBATCH --ntasks-per-node=32
 #SBATCH --ntasks=256
 #SBATCH --ntasks-per-socket=16
-#SBATCH --time=3-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH -e job%J.err
 #SBATCH -o job%J.out
-#SBATCH --partition=72hours
-#SBATCH --qos=72hours
+#SBATCH --partition=workq
+##SBATCH --partition=72hours
+##SBATCH --qos=72hours
 
 OMP_NUM_THREADS=1
+
+module load python/3.6.2
+AIRINLET="AIR1 AIR2 AIR3 AIR4"
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    cp /project/k1242/Zhen/OpenFOAM/OpenFOAM_py/python3.6/process/inlet_time_ext.py .
+    cd ..
+done
+cd ../..
 
 canteraToFoam
 sed "s/@NUMSUBDOM@/256/g" \
@@ -30,7 +43,7 @@ srun --hint=nomultithread flameletFoam -parallel
 
 cp system/controlDict system/log1_controlDict
 sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.01/g" \
-    -e "s/@DELTAT@/1e-6/g" -e "s/@WRITEINTERVAL@/0.009/g" \
+    -e "s/@DELTAT@/1e-6/g" -e "s/@WRITEINTERVAL@/0.001/g" \
     -e "s/@WRITEFORMAT@/binary/g" \
     -e "s/@ENABLED@/false/g" \
     -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
--- SwBs_PIMPLE_WF/run_shaheen.sh
+++ SwBs_PIMPLE_WF/run_shaheen.sh
@@ -1,73 +1,54 @@
 #!/bin/bash
 #SBATCH --account=k1242
-#SBATCH --job-name=SwBsNRWFL3
-#SBATCH --nodes=8
+#SBATCH --job-name=SwBsNRWFL2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
-#SBATCH --ntasks=256
 #SBATCH --ntasks-per-socket=16
-#SBATCH --time=3-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH -e job%J.err
 #SBATCH -o job%J.out
-#SBATCH --partition=72hours
-#SBATCH --qos=72hours
+#SBATCH --partition=workq
+##SBATCH --partition=72hours
+##SBATCH --qos=72hours
 
 OMP_NUM_THREADS=1
 
 module load python/3.6.2
 AIRINLET="AIR1 AIR2 AIR3 AIR4"
 
-#cd constant/boundaryData
-#for folder in $AIRINLET
-#do
-#    cd $folder
-#    cp /project/k1242/Zhen/OpenFOAM/OpenFOAM_py/python3.6/process/inlet_time_ext.py .
-#    cd ..
-#done
-#cd ../..
-#
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    cp /project/k1242/Zhen/OpenFOAM/OpenFOAM_py/python3.6/process/inlet_time_ext.py .
+    cd ..
+done
+cd ../..
 
-#sed "s/@NUMSUBDOM@/256/g" \
-#    system/decomposeParDict_template > system/decomposeParDict
-#decomposePar
-#
-#sed -e "s/@STARTTIME@/startTime/g" -e "s/@ENDTIME@/0.001/g" \
-#    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.001/g" \
-#    -e "s/@WRITEFORMAT@/binary/g" \
-#    -e "s/@ENABLED@/false/g"  \
-#    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
-#    system/controlDict_template > system/controlDict
-#srun --hint=nomultithread pimpleFoam -parallel
-#
-#cp system/controlDict system/log1_controlDict
-#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.01/g" \
-#    -e "s/@DELTAT@/5e-6/g" -e "s/@WRITEINTERVAL@/0.009/g" \
-#    -e "s/@WRITEFORMAT@/binary/g" \
-#    -e "s/@ENABLED@/false/g" \
-#    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
-#    system/controlDict_template > system/controlDict
-#srun --hint=nomultithread pimpleFoam -parallel
-#
-#cp system/controlDict system/log2_controlDict
-#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.10/g" \
-#    -e "s/@DELTAT@/5e-6/g" -e "s/@WRITEINTERVAL@/0.01/g" \
-#    -e "s/@WRITEFORMAT@/binary/g" \
-#    -e "s/@ENABLED@/false/g" \
-#    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
-#    system/controlDict_template > system/controlDict
-#srun --hint=nomultithread pimpleFoam -parallel
-#
-#cd constant/boundaryData
-#for folder in $AIRINLET
-#do
-#    cd $folder
-#    python3 inlet_time_ext.py
-#    cd ..
-#done
-#cd ../..
-#
-cp system/controlDict system/log3_controlDict
-sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.2/g" \
-    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+sed "s/@NUMSUBDOM@/32/g" \
+    system/decomposeParDict_template > system/decomposeParDict
+decomposePar
+
+sed -e "s/@STARTTIME@/startTime/g" -e "s/@ENDTIME@/0.001/g" \
+    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.001/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g"  \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cp system/controlDict system/log1_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.01/g" \
+    -e "s/@DELTAT@/5e-6/g" -e "s/@WRITEINTERVAL@/0.009/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cp system/controlDict system/log2_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.10/g" \
+    -e "s/@DELTAT@/5e-6/g" -e "s/@WRITEINTERVAL@/0.01/g" \
     -e "s/@WRITEFORMAT@/binary/g" \
     -e "s/@ENABLED@/false/g" \
     -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
@@ -83,13 +64,11 @@ do
 done
 cd ../..
 
-# START TAKING AVERAGES
-
-cp system/controlDict system/log4_controlDict
-sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.3/g" \
-    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+cp system/controlDict system/log3_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.2/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.02/g" \
     -e "s/@WRITEFORMAT@/binary/g" \
-    -e "s/@ENABLED@/true/g" \
+    -e "s/@ENABLED@/false/g" \
     -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
     system/controlDict_template > system/controlDict
 srun --hint=nomultithread pimpleFoam -parallel
@@ -102,14 +81,339 @@ do
     cd ..
 done
 cd ../..
-#
-#
-## CONTINUE AVERAGING
-#cp system/controlDict system/log5_controlDict
-#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.25/g" \
-#    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
-#    -e "s/@WRITEFORMAT@/binary/g" \
-#    -e "s/@ENABLED@/true/g" \
-#    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
-#    system/controlDict_template > system/controlDict
-#srun --hint=nomultithread pimpleFoam -parallel
+
+cp system/controlDict system/log4_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.3/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+ CONTINUE AVERAGING
+cp system/controlDict system/log5_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.4/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log6_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.5/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log7_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.6/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log8_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.7/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log9_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.8/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log10_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.9/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log11_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.0/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log12_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.1/g" \
+    -e "s/@DELTAT@/0.000005/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log13_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.2/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log14_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.3/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log15_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.4/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log16_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.5/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log17_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.6/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log18_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.7/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log19_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.8/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log20_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/1.9/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log21_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/2.0/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.05/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+# transient data for animation
+
+cp system/controlDict system/log22_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/2.01/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.0001/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+srun --hint=nomultithread pimpleFoam -parallel
