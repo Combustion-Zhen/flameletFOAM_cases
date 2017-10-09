13ade47cfba545a3520efe838a512f62a4e64c4d
Zhen <zhen.lu@kaust.edu.sa>
2017-10-09 10:33:22 +0300

    time precision in controlDict

--- SwBd_FLAMELET_WF/run_shaheen.sh
+++ SwBd_FLAMELET_WF/run_shaheen.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --account=k1242
-#SBATCH --job-name=SwBdFLTL4
-#SBATCH --nodes=8
+#SBATCH --job-name=SwBdFLTL3
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=32
-#SBATCH --ntasks=256
 #SBATCH --ntasks-per-socket=16
 #SBATCH --time=1-00:00:00
 #SBATCH -e job%J.err
@@ -17,85 +16,188 @@ OMP_NUM_THREADS=1
 module load python/3.6.2
 AIRINLET="AIR1 AIR2 AIR3 AIR4"
 
-cd constant/boundaryData
-for folder in $AIRINLET
-do
-    cd $folder
-    cp /project/k1242/Zhen/OpenFOAM/OpenFOAM_py/python3.6/process/inlet_time_ext.py .
-    cd ..
-done
-cd ../..
-
-canteraToFoam
-sed "s/@NUMSUBDOM@/256/g" \
-    system/decomposeParDict_template > system/decomposeParDict
-decomposePar
-
-sed -e "s/@STARTTIME@/startTime/g" -e "s/@ENDTIME@/0.001/g" \
-    -e "s/@DELTAT@/1e-7/g" -e  "s/@WRITEINTERVAL@/0.001/g" \
-    -e "s/@WRITEFORMAT@/binary/g" \
-    -e "s/@ENABLED@/false/g"  \
-    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
-    system/controlDict_template > system/controlDict
-sed -e "s/@DIVPHIU@/Gauss vanLeer/g" \
-    system/fvSchemes_template > system/fvSchemes
-srun --hint=nomultithread flameletFoam -parallel
-
-cp system/controlDict system/log1_controlDict
-sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.01/g" \
-    -e "s/@DELTAT@/1e-6/g" -e "s/@WRITEINTERVAL@/0.001/g" \
-    -e "s/@WRITEFORMAT@/binary/g" \
-    -e "s/@ENABLED@/false/g" \
-    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
-    system/controlDict_template > system/controlDict
-sed -e "s/@DIVPHIU@/Gauss vanLeer/g" \
-    system/fvSchemes_template > system/fvSchemes
-srun --hint=nomultithread flameletFoam -parallel
-
-cp system/controlDict system/log2_controlDict
-sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.10/g" \
-    -e "s/@DELTAT@/1e-6/g" -e "s/@WRITEINTERVAL@/0.01/g" \
-    -e "s/@WRITEFORMAT@/binary/g" \
-    -e "s/@ENABLED@/false/g" \
-    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
-    system/controlDict_template > system/controlDict
-sed -e "s/@DIVPHIU@/Gauss vanLeer/g" \
-    system/fvSchemes_template > system/fvSchemes
-srun --hint=nomultithread flameletFoam -parallel
-
-#cp system/controlDict system/log3_controlDict
-#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.15/g" \
-#    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+#cd constant/boundaryData
+#for folder in $AIRINLET
+#do
+#    cd $folder
+#    cp /project/k1242/Zhen/OpenFOAM/OpenFOAM_py/python3.6/process/inlet_time_ext.py .
+#    cd ..
+#done
+#cd ../..
+#
+#canteraToFoam
+#sed "s/@NUMSUBDOM@/96/g" \
+#    system/decomposeParDict_template > system/decomposeParDict
+#decomposePar -latestTime
+#
+#sed -e "s/@STARTTIME@/startTime/g" -e "s/@ENDTIME@/0.001/g" \
+#    -e "s/@DELTAT@/1e-7/g" -e  "s/@WRITEINTERVAL@/0.001/g" \
+#    -e "s/@WRITEFORMAT@/binary/g" \
+#    -e "s/@ENABLED@/false/g"  \
+#    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+#    system/controlDict_template > system/controlDict
+#sed -e "s/@DIVPHIU@/Gauss vanLeer/g" \
+#    system/fvSchemes_template > system/fvSchemes
+#srun --hint=nomultithread flameletFoam -parallel
+#
+#cp system/controlDict system/log1_controlDict
+#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.01/g" \
+#    -e "s/@DELTAT@/1e-6/g" -e "s/@WRITEINTERVAL@/0.001/g" \
 #    -e "s/@WRITEFORMAT@/binary/g" \
 #    -e "s/@ENABLED@/false/g" \
 #    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
 #    system/controlDict_template > system/controlDict
-#sed -e "s/@DIVPHIU@/Gauss linear/g" \
+#sed -e "s/@DIVPHIU@/Gauss vanLeer/g" \
 #    system/fvSchemes_template > system/fvSchemes
 #srun --hint=nomultithread flameletFoam -parallel
 #
-## START TAKING AVERAGES
-#
-#cp system/controlDict system/log4_controlDict
-#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.20/g" \
-#    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+#cp system/controlDict system/log2_controlDict
+#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.10/g" \
+#    -e "s/@DELTAT@/5e-6/g" -e "s/@WRITEINTERVAL@/0.01/g" \
 #    -e "s/@WRITEFORMAT@/binary/g" \
-#    -e "s/@ENABLED@/true/g" \
+#    -e "s/@ENABLED@/false/g" \
 #    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
 #    system/controlDict_template > system/controlDict
-#sed -e "s/@DIVPHIU@/Gauss linear/g" \
+#sed -e "s/@DIVPHIU@/Gauss vanLeer/g" \
 #    system/fvSchemes_template > system/fvSchemes
 #srun --hint=nomultithread flameletFoam -parallel
 #
+#cd constant/boundaryData
+#for folder in $AIRINLET
+#do
+#    cd $folder
+#    python3 inlet_time_ext.py
+#    cd ..
+#done
+#cd ../..
 #
-## CONTINUE AVERAGING
-#cp system/controlDict system/log5_controlDict
-#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.25/g" \
-#    -e "s/@DELTAT@/1e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+#cp system/controlDict system/log3_controlDict
+#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.20/g" \
+#    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
 #    -e "s/@WRITEFORMAT@/binary/g" \
-#    -e "s/@ENABLED@/true/g" \
+#    -e "s/@ENABLED@/false/g" \
+#    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+#    system/controlDict_template > system/controlDict
+#sed -e "s/@DIVPHIU@/Gauss vanLeer/g" \
+#    system/fvSchemes_template > system/fvSchemes
+#srun --hint=nomultithread flameletFoam -parallel
+#
+#cd constant/boundaryData
+#for folder in $AIRINLET
+#do
+#    cd $folder
+#    python3 inlet_time_ext.py
+#    cd ..
+#done
+#cd ../..
+#
+#cp system/controlDict system/log4_controlDict
+#sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.3/g" \
+#    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+#    -e "s/@WRITEFORMAT@/binary/g" \
+#    -e "s/@ENABLED@/false/g" \
 #    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
 #    system/controlDict_template > system/controlDict
-#sed -e "s/@DIVPHIU@/Gauss linear/g" \
+#sed -e "s/@DIVPHIU@/Gauss vanLeer/g" \
 #    system/fvSchemes_template > system/fvSchemes
 #srun --hint=nomultithread flameletFoam -parallel
+#
+#cd constant/boundaryData
+#for folder in $AIRINLET
+#do
+#    cd $folder
+#    python3 inlet_time_ext.py
+#    cd ..
+#done
+#cd ../..
+#
+cp system/controlDict system/log5_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.4/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.01/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+sed -e "s/@DIVPHIU@/Gauss linear/g" \
+    system/fvSchemes_template > system/fvSchemes
+srun --hint=nomultithread flameletFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log6_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.5/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.02/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+sed -e "s/@DIVPHIU@/Gauss linear/g" \
+    system/fvSchemes_template > system/fvSchemes
+srun --hint=nomultithread flameletFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log7_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.6/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.02/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/false/g" \
+    -e "s/@RESTART@/false/g" -e "s/@RESTARTOUT@/false/g" \
+    system/controlDict_template > system/controlDict
+sed -e "s/@DIVPHIU@/Gauss linear/g" \
+    system/fvSchemes_template > system/fvSchemes
+srun --hint=nomultithread flameletFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log8_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.7/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.02/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+sed -e "s/@DIVPHIU@/Gauss linear/g" \
+    system/fvSchemes_template > system/fvSchemes
+srun --hint=nomultithread flameletFoam -parallel
+
+cd constant/boundaryData
+for folder in $AIRINLET
+do
+    cd $folder
+    python3 inlet_time_ext.py
+    cd ..
+done
+cd ../..
+
+cp system/controlDict system/log9_controlDict
+sed -e "s/@STARTTIME@/latestTime/g" -e "s/@ENDTIME@/0.8/g" \
+    -e "s/@DELTAT@/5e-6/g" -e  "s/@WRITEINTERVAL@/0.02/g" \
+    -e "s/@WRITEFORMAT@/binary/g" \
+    -e "s/@ENABLED@/true/g" \
+    -e "s/@RESTART@/true/g" -e "s/@RESTARTOUT@/true/g" \
+    system/controlDict_template > system/controlDict
+sed -e "s/@DIVPHIU@/Gauss linear/g" \
+    system/fvSchemes_template > system/fvSchemes
+srun --hint=nomultithread flameletFoam -parallel
